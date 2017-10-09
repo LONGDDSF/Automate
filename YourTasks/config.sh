4a998bec76b57c695cfb9e4b9e5a5040d1665f24
chendongdong <chendongdong@syswin.com>
2017-10-09 18:09:16 +0800

    bug fix

--- YourTasks/config.sh
+++ YourTasks/config.sh
@@ -1,16 +1,16 @@
 #!bin/sh
 
 #私有pod库本地根目录
-readonly path_pod_libs="/Users/syswin/Documents/Siyuan/Libs"
+readonly path_pod_libs=""
 
 #主项目本地根目录
-readonly path_main_project="/Users/syswin/Documents/Siyuan/ToonProject/toonios/Toon"
+readonly path_main_project=""
 
 #私有repo的http地址
-readonly url_private_repo="http://172.28.6.24:8080/syswin_pod_spec"
+readonly url_private_repo=""
 
 #cocoapods
-readonly url_cocoapods_repo="https://github.com/CocoaPods/Specs.git"
+readonly url_cocoapods_repo=""
 
 # export "&path_siyuan_lib"
 # export "$path_toon"
