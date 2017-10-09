62d9f8f9fabaac698141a0a69fdc99d324bdaaec
chendongdong <chendongdong@syswin.com>
2017-10-09 18:22:29 +0800

    config

---
+++ YourTasks/config.sh
@@ -0,0 +1,13 @@
+#!bin/sh
+
+#私有pod库本地根目录
+readonly path_pod_libs=""
+
+#主项目本地根目录
+readonly path_main_project=""
+
+#私有repo的http地址
+readonly url_private_repo=""
+
+#cocoapods
+readonly url_cocoapods_repo=""
