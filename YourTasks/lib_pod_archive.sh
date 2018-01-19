72bbaae9ee6bbdf62597e1837f78a75661caaeb9
chendongdong <chendongdong@syswin.com>
2018-01-19 18:47:54 +0800

    bug fix

--- YourTasks/lib_pod_archive.sh
+++ YourTasks/lib_pod_archive.sh
@@ -15,6 +15,15 @@ function f_selTarget
 	echo $targetName
 }
 
+#function f_sel_tag
+#{
+#    (f_echo)
+#
+#    read -p "请输入tag :" targetTag
+#
+#    echo $targetTag
+#}
+
 function doWork
 {
 	cd $rootPath
@@ -23,6 +32,8 @@ function doWork
 	(f_file_list $rootPath)
 
 	'f_selTarget'
+
+#    `f_sel_tag`
 
 	#校验目标路径
 	(f_checkFolderIsAviliable $targetName)
@@ -43,14 +54,13 @@ function doWork
 				(f_git_stash $rootPath/$targetName)
 
 				(f_privite_repo_cache_clean)
-				
+
 				(f_echo '开始打包 .....')
 
 				podSpecName=$targetName.podspec
-
-				#当前pod要使用源码
-				eval "${targetName}_use_code=1" eval "$privitePodSourceParam"\
-				pod package $podSpecName --no-mangle --exclude-deps --force \
+				#当前pod要使用源码use_tag=`<`\"$targetTag\"
+				eval "${targetName}_use_code=1" eval "$privitePodSourceParam" \
+                pod package $podSpecName --no-mangle --exclude-deps --force \
 				--spec-sources=$url_private_repo,$url_cocoapods_repo
 		
 				(f_echo "打包结束 。。。。$podSpecName ")
